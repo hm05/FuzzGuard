61be0eddf7270356cefe2c32063eb6d8b4a09392
hm05 <harshmurjani5114@gmail.com>
2024-05-15 21:50:02 +0530

    FuzzMySql

--- FuzzMySql.py
+++ FuzzMySql.py
@@ -8,51 +8,31 @@ def connectMySQL(host, user, password):
             user=user,
             password=password
         )
-        print(f'[\033[92m -\033[0m ] Username : {user}, Password : {password}')
+        print(f'[\033[92m + \033[0m] Successfully connected to MySQL database: {user}@{host}')
         return mydb
     except mysql.connector.Error as err:
-        print(f'[\033[91m -\033[0m ] Failed to connect to MySQL database: {err}')
+        print(f'[\033[91m - \033[0m] Failed to connect to MySQL database: {err}')
         return None
     except KeyboardInterrupt:
-        print('[\033[91m -\033[0m ] Detecting Keyboard Interrupt...Exiting...')
+        print('[\033[91m - \033[0m] Detecting Keyboard Interrupt...Exiting...')
         exit(1)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MySQL Connector')
-    user = parser.add_mutually_exclusive_group(required=True)
-    user.add_argument('-u', '--user', type=str, help='Username')
-    user.add_argument('-U', '--userfile', type=argparse.FileType('r'), help='User File')
-    password = parser.add_mutually_exclusive_group(required=True)
-    password.add_argument('-p', '--password', type=str, help='Password')
-    password.add_argument('-P', '--passfile', type=argparse.FileType('r'), help='Password File')
+    parser.add_argument('-u', '--user', type=str, help='Username')
+    parser.add_argument('-U', '--userfile', type=argparse.FileType('r'), help='User File')
+    parser.add_argument('-p', '--password', type=str, help='Password')
+    parser.add_argument('-P', '--passfile', type=argparse.FileType('r'), help='Password File')
     parser.add_argument('-H', '--host', type=str, help='Host')
     args = parser.parse_args()
-    try:
-        if args.user:
-            if args.passwordfile:
-                mydb = mysql.connector.connect(
-                host=args.host,
-                user=args.user,
-                password=args.passwordfile.readline()
-                )
-            else:
-                mydb = mysql.connector.connect(
-                host=args.host,
-                user=args.user,
-                password=args.password
-                )
-        else:
-            if args.password:
-                mydb = mysql.connector.connect(
-                host=args.host,
-                user=args.userfile.readline(),
-                password=args.password
-                )
-            elif args.passwordfile:
-                mydb = mysql.connector.connect(
-                host=args.host,
-                user=args.userfile.readline(),
-                password=args.passwordfile.readline()
-                )
-    except FileNotFoundError:
-        print(f"Error: user or password file not found.")
+
+    if not args.user and not args.userfile:
+        parser.error('Please provide a username or a file containing usernames.')
+
+    if not args.password and not args.passfile:
+        parser.error('Please provide a password or a file containing passwords.')
+
+    user = args.user if args.user else args.userfile.readline().strip()
+    password = args.password if args.password else args.passfile.readline().strip()
+
+    mydb = connectMySQL(args.host, user, password)
